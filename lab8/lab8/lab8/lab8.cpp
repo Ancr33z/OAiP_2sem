feb820d04860cee9e04d9b3f5d3250eecf9e0e92
Ancr33z <nikita.borisov20011@gmail.com>
2024-02-07 21:06:42 +0300

    .

--- lab8/lab8/lab8/lab8.cpp
+++ lab8/lab8/lab8/lab8.cpp
@@ -8,11 +8,15 @@ struct Number
 void create(Number** begin, Number** end, int p); //формирование элементов очереди
 void view(Number* begin);                        //вывод элементов очереди 
 Number* minElem(Number* begin);               //определение минимального элемента 
-void DeltoMin(Number** begin, Number** p);    //удаление до минимального элемента 
+Number* maxElem(Number* begin);
+int elemBetween(Number* min, Number* max);
+void delMaxElem(Number** begin);
+
 int main()
 {
-    Number* begin = NULL, * end, * t;
+    Number* begin = NULL, * end, * t, * max;
     t = new Number;
+    max = new Number;
     int p, size;
     cout << "\nEnter size queue=";  cin >> size;
     cout << "Enter number= ";       cin >> p;
@@ -31,9 +35,13 @@ int main()
         view(begin);
     t = minElem(begin);    //определение минимального
     cout << "minimum=" << t->info << endl;
-    DeltoMin(&begin, &t);   //удаление до минимального
-    cout << "\nnew Queue:\n";
+    max = maxElem(begin);
+    cout << "maximum=" << max->info << endl;
+    cout << "Element between min and max elements: " << elemBetween(t, max);
+    delMaxElem(&begin);
+    cout << "\n Max element deleted \n";
     view(begin);
+
     return 0;
 }
 void create(Number** begin, Number** end, int p) //Формирование элементов очереди
@@ -87,14 +95,78 @@ Number* minElem(Number* begin) //Определение минимального
     }
     return mn;
 }
-void DeltoMin(Number** begin, Number** p) //Удаление до минимального элемента 
+Number* maxElem(Number* begin)
 {
-    Number* t;
-    t = new Number;
-    while (*begin != *p)
+    Number* t = begin, * mx = nullptr;
+    int max;
+    if (t == NULL)
     {
-        t = *begin;
+        cout << "Number is empty\n"; return 0;
+    }
+    else
+    {
+        max = t->info;
+        while (t != NULL)
+        {
+            if (t->info >= max)
+            {
+                max = t->info;
+                mx = t;
+            }
+            t = t->next;
+        }
+    }
+    return mx;
+}
+
+int elemBetween(Number* min, Number* max)
+{
+    Number* t = min;
+    int count = 0;
+    while (t != NULL)
+    {
+        if (t == max)
+        {
+            return (count - 1);
+        }
+        else
+        {
+            t = t->next;
+            count++;
+        }
+    }
+}
+
+void delMaxElem(Number** begin)
+{
+    if (*begin == nullptr) {
+        // Список пуст, ничего не удаляем
+        return;
+    }
+
+    // Ищем максимальный элемент в списке
+    Number* max = *begin;
+    Number* current = (*begin)->next;
+
+    while (current != nullptr) {
+        if (current->info > max->info) {
+            max = current;
+        }
+        current = current->next;
+    }
+
+    // Удаляем максимальный элемент
+    current = *begin;
+    if (*begin == max) {
+        // Если максимальный элемент - первый в списке
         *begin = (*begin)->next;
-        delete t;
+        delete max;
+    }
+    else {
+        while (current->next != max) {
+            current = current->next;
+        }
+        current->next = max->next;
+        delete max;
     }
 }
