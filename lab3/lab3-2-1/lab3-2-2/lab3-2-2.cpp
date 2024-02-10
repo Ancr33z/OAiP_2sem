fe3b26fcfe5e17317e36c76fd47e4ee93e568df3
Ancr33z <nikita.borisov20011@gmail.com>
2024-02-10 10:25:15 +0300

    .

--- lab3/lab3-2-1/lab3-2-2/lab3-2-2.cpp
+++ lab3/lab3-2-1/lab3-2-2/lab3-2-2.cpp
@@ -3,19 +3,37 @@
 
 using namespace std;
 
+void ifFile(char inputString[256]);
+void fromFile();
+
+char smallest[256];
+
 int main()
 {
     setlocale(LC_ALL, "rus");
     int i = 0;
-    char buff[256];
-    char smallest[256];
-    ofstream file1("file1.txt");
-    char currentChar;
-
     char inputString[256];
     cout << "Введите строку символов: ";
     cin.getline(inputString, sizeof(inputString));
-    while(inputString[i] != '\0')
+
+
+
+    ifFile(inputString);
+
+    fromFile();
+   
+
+
+    cout << '\n' << "Самая короткая группа: " << smallest << '\n';
+    cout << "\nПрограмма успешно выполнена";
+    return 0;
+}
+
+void ifFile(char inputString[256]) 
+{
+    int i = 0;
+    ofstream file1("file1.txt");
+    while (inputString[i] != '\0')
     {
         if (inputString[i] != '0')
             file1 << inputString[i];
@@ -28,21 +46,30 @@ int main()
     }
 
     file1.close();
+}
 
+void fromFile()
+{
     ifstream outFile("file1.txt");
+
+    char buff[256];
+    char currentChar;
+
+    outFile >> buff;
+    strcpy_s(smallest, buff);
 
     do
     {
         outFile >> buff;
 
-        if (strlen(buff) < strlen(smallest))
+        if (strlen(buff) < strlen(smallest) && buff[0] != '\0')
         {
             strcpy_s(smallest, buff);
-             
+
         }
+
     } while (outFile.get(currentChar));
 
-    cout << '\n' << "Самая короткая группа: " << smallest << '\n';
-    cout << "\nПрограмма успешно выполнена";
-    return 0;
+    outFile.close();
+
 }
